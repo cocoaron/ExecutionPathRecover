0383f163dcfc1d29d3ab0d438e1388778d1e8290
MINGYU KIM <104493794+cocoaron@users.noreply.github.com>
2024-10-23 17:04:07 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+CURRENT_DIR=$(pwd)
+SCRIPT_PATH="$CURRENT_DIR/PathUp/pathup_run.sh"
+
+if [ -f "$SCRIPT_PATH" ]; then
+    # Navigate to PathUp and run pathup_run.sh
+    cd "$CURRENT_DIR/PathUp" || { echo "Failed to navigate to $CURRENT_DIR/PathUp"; exit 1; }
+    ./pathup_run.sh
+else
+    echo "chkup_run.sh not found in $CURRENT_DIR/PathUp"
+    exit 1
+fi
 
