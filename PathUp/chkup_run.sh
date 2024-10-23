905be557db4a464eefe70e1f155af56f588a4945
MINGYU KIM <104493794+cocoaron@users.noreply.github.com>
2024-10-23 17:02:00 +0900

    Update chkup_run.sh

--- PathUp/chkup_run.sh
+++ PathUp/chkup_run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash 
 
+# Get the current working directory
+CURRENT_DIR=$(pwd)
+
 # Initialize the JavaScript parser
 is_npm_script_running() {
     pgrep -fl "node.*$1" > /dev/null
 }
 
 SCRIPT_NAME="start"
-SCRIPT_PATH="/PathUp/exec_pth_rec/jsparse"
+SCRIPT_PATH="$CURRENT_DIR/exec_pth_rec/jsparse"
 
 cd "$SCRIPT_PATH" || { echo "Failed to navigate to $SCRIPT_PATH"; exit 1; }
 
@@ -20,16 +23,16 @@ fi
 
 # Define an array of testing firmware paths
 file_paths=(
-    "/firmware/extracted/RLC-410W_extracted"
-    "/firmware/extracted/wa801nv1_en_3_12_6_up_bin_extracted"
-    "/firmware/extracted/_WNR1000v2_V1_1_2_60NA_img_extracted"
+    "$CURRENT_DIR/firmware/extracted/RLC-410W_extracted"
+    "$CURRENT_DIR/firmware/extracted/wa801nv1_en_3_12_6_up_bin_extracted"
+    "$CURRENT_DIR/firmware/extracted/_WNR1000v2_V1_1_2_60NA_img_extracted"
 )
 
 # Loop through the array of file paths
-WORKING_DIR="/PathUp"
+WORKING_DIR="$CURRENT_DIR/PathUp"
 cd "$WORKING_DIR" || { echo "Failed to navigate to $WORKING_DIR"; exit 1; }
 
 for file_path in "${file_paths[@]}"
 do
-    python3 main.py --firmware_path "$file_path" --results_path "./results"
+    python3 main.py --firmware_path "$file_path" --results_path "$CURRENT_DIR/results"
 done
